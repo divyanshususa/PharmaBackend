61091f65e17f5ba34bbfd647364a3a75781a2224
anujsinghsusalabs <anuj.singh@susalabs.com>
2024-11-08 12:46:57 +0530

    login credentials

--- src/dbconnect/index.js
+++ src/dbconnect/index.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
- 
-const connectDB = async()=>{ 
-try {
-   const ConnectionInstance = await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-console.log(`\n MONGO DB CONNECTED !! DB HOST  : ${ConnectionInstance.connection.host}`)
-} catch (error) {
-    console.error('Error',error)
-    process.exit(1);
-}
 
-}
+const connectDB = async () => {
+    try {
+        const ConnectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`\nMONGO DB CONNECTED !! DB HOST  : ${ConnectionInstance.connection.host}`);
+    } catch (error) {
+        console.error('Error', error);
+        process.exit(1);
+    }
+};
 
-
-export default connectDB
+export default connectDB;
